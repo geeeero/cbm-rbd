8962b19a105bdf0fea5f2223925069270a3e26ec
Gero Walter <gero@gmx.at>
2016-11-05 23:09:23 +0100

    campi-slides uncovering piecewise with corresponding plots

--- campi-plots.R
+++ campi-plots.R
@@ -44,37 +44,52 @@ sysABfts <- list(A1 = NULL, A2 = 5, B = NULL)
 sysABn0y0 <- list(A = compA$n0y0$A, B = compB$n0y0$B)
 sysABrelhist05 <- sysrelnowhist(sysAB, sysABtypes, sysABfts, sysABn0y0, c(betaA, betaB), c(0,5), hor=15)
 pdf("talk-campi/sysABrel0.pdf", width = 1.5*minirelsize, height = minirelsize)
-ggplot(subset(sysABrelhist05, tnow == 0), aes(x = t, y = rel)) + geom_line() + minirel
+ggplot(subset(sysABrelhist05, tnow == 0), aes(x = t, y = rel)) + geom_line(size = 0.8) + minirel
 dev.off()
 pdf("talk-campi/sysABrel5.pdf", width = 1.5*minirelsize, height = minirelsize)
-ggplot(subset(sysABrelhist05, tnow == 5), aes(x = t, y = rel)) + geom_line() + minirel
+ggplot(subset(sysABrelhist05, tnow == 5), aes(x = t, y = rel)) + geom_line(size = 0.8) + minirel
 dev.off()
 
-sysABghist <- gnowhist(sysAB, sysABtypes, sysABfts, sysABn0y0, c(betaA, betaB), seq(0, 10, by=2.5), hor = 15, seqlen = 301)
-sysABghist$tnow <- as.factor(sysABghist$tnow)
+sysABtnowseq <- seq(0, 10, by=2.5)
+sysABghist <- gnowhist(sysAB, sysABtypes, sysABfts, sysABn0y0, c(betaA, betaB), sysABtnowseq, hor = 15, seqlen = 301)
+sysABghist$tnow <- factor(sysABghist$tnow, levels = sysABtnowseq)
+sysABtauhist <- taustarhist(sysAB, sysABtypes, sysABfts, sysABn0y0, c(betaA, betaB), sysABtnowseq, hor = 5, seqlen = 501)
+sysABtauhist$tnowf <- factor(sysABtauhist$tnow, levels = sysABtnowseq)
 
-pdf("talk-campi/sysABrelhist5.pdf", width = 5.3, height = 1.6)
-ggplot(sysABghist, aes(x = t, y = rel)) + coord_cartesian(xlim = c(0,25), ylim = c(0,1)) + xlab(expression(t)) +
-  geom_line(aes(group = tnow, colour = tnow)) + #ylab(expression(paste(R[sys]^(t[now]), (t),))) +
-  theme(axis.title = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-  #guides(colour=guide_legend(title=expression(t[now]))) + 
-  guides(colour = 'none') +
-  scale_y_continuous(breaks=c(0, 0.5, 1))
-dev.off()
+for(i in 1:length(sysABtnowseq)){
+  pdf(paste("talk-campi/sysABrelhist", i, ".pdf", sep=""), width = 5.3, height = 1.6)
+  print(#
+  ggplot(subset(sysABghist, tnow %in% sysABtnowseq[1:i]), aes(x = t, y = rel)) + 
+    coord_cartesian(xlim = c(0,25), ylim = c(0,1)) + xlab(expression(t)) +
+    geom_line(aes(group = tnow, colour = tnow), size = 0.8) + #ylab(expression(paste(R[sys]^(t[now]), (t),))) +
+    theme(axis.title = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    #guides(colour=guide_legend(title=expression(t[now]))) + 
+    guides(colour = 'none') +
+    scale_y_continuous(breaks=c(0, 0.5, 1)) + scale_colour_discrete(drop = FALSE))
+  dev.off()
+  pdf(paste("talk-campi/sysABghist", i, ".pdf", sep=""), width = 6, height = 1.7)
+  print(#
+  ggplot(subset(sysABghist, tnow %in% sysABtnowseq[1:i]), aes(x = t, y = gnow)) + 
+    coord_cartesian(xlim = c(0,25), ylim = c(0,0.4)) + xlab(expression(t)) +
+    geom_line(aes(group = tnow, colour = tnow), size = 0.8) + #ylab(expression(paste(g^(t[now]), (t),))) +
+    theme(axis.title = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    theme(axis.title.y = element_blank()) + guides(colour=guide_legend(title=expression(t[now]))) +
+    scale_y_continuous(breaks=seq(0.1, 0.4, 0.1)) + scale_colour_discrete(drop = FALSE))
+  dev.off()
+  pdf(paste("talk-campi/sysABtau", i, ".pdf", sep=""), width = 4.5, height = 1)
+  print(#
+  ggplot(subset(sysABtauhist, tnow %in% sysABtnowseq[1:i]), aes(x = tnow, y = tstar, ymin = tnow, ymax = tstar, colour = tnowf)) +
+    geom_linerange(size=1.5) + scale_x_continuous(breaks=seq(0, 10, 2.5), minor_breaks = NULL) + 
+    coord_flip(xlim = c(0,10), ylim = c(0,25)) + ylab("t") +
+    #guides(colour=guide_legend(title=expression(t[now]))) +
+    guides(colour = 'none') + #scale_colour_discrete(drop = FALSE) + 
+    theme(axis.title = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+    scale_colour_discrete(drop = FALSE))
+  dev.off()
+}
 
-pdf("talk-campi/sysABghist5.pdf", width = 6, height = 1.7)
-ggplot(sysABghist, aes(x = t, y = gnow)) + coord_cartesian(xlim = c(0,25), ylim = c(0,0.4)) + xlab(expression(t)) +
-  geom_line(aes(group = tnow, colour = tnow)) + #ylab(expression(paste(g^(t[now]), (t),))) +
-  theme(axis.title = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-  theme(axis.title.y = element_blank()) + guides(colour=guide_legend(title=expression(t[now]))) +
-  scale_y_continuous(breaks=seq(0.1, 0.4, 0.1))
-dev.off()
-
-sysABtauhist <- taustarhist(sysAB, sysABtypes, sysABfts, sysABn0y0, c(betaA, betaB), seq(0, 10, by=2.5), hor = 5, seqlen = 501)
-sysABtauhist$tnowf <- as.factor(sysABtauhist$tnow)
-
-pdf("talk-campi/sysABtau5.pdf", width = 4.5, height = 1)
-ggplot(sysABtauhist, aes(x = tnow, y = tstar, ymin = tnow, ymax = tstar, colour = tnowf)) + geom_linerange(size=1.5) +
+pdf("talk-campi/sysABtau5a.pdf", width = 4.5, height = 1)
+ggplot(sysABtauhist, aes(x = tnow, y = tstar, ymin = rep(0, length(tnow)), ymax = taustar, colour = tnowf)) + geom_linerange(size=1.5) +
   scale_x_continuous(breaks=seq(0, 10, 2.5), minor_breaks = NULL) + coord_flip(ylim = c(0,25)) + ylab("t") +
   #guides(colour=guide_legend(title=expression(t[now]))) +
   guides(colour = 'none') +
