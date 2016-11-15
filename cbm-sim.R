0e110fbd51ff8717dfa6ac01916dfbd59a122bee
Gero Walter <gero@gmx.at>
2016-11-15 20:52:18 +0100

    debug sim1cycle

--- cbm-sim.R
+++ cbm-sim.R
@@ -19,7 +19,7 @@
 # cu       cost of unplanned (corrective) repair action
 # cp       cost of planned (preventive) repair action, cp < cu
 # onecycle whether to use the one-cycle criterion or the renewal-based one
-sim1cycle <- function(sys, ctypes, compfts, n0y0, beta, fts, tnowstep, hor, tprep, trepa = 0,
+sim1cycle <- function(sys, ctypes, compfts, n0y0, beta, tnowstep, hor, tprep, trepa = 0,
                       seqlen = 101, prior = FALSE, cu = 1, cp = 0.2, onecycle = TRUE){
   K <- length(ctypes)
   ftschron <- sort(unlist(compfts)) # when something fails
@@ -34,6 +34,7 @@ sim1cycle <- function(sys, ctypes, compfts, n0y0, beta, fts, tnowstep, hor, tpre
   repschedfor = Inf   # for which time is repair scheduled?
   res <- data.frame() # initialize results data frame
   while(gotonext){
+    cat("tnow =", tnow,"\n")
     if (!failed){ # if not failed already in previous loop, check...
       if (all(signnow$Probability == 0)){ # system has failed now, schedule repair for tnow + tprep if not scheduled already
         failed <- TRUE
@@ -45,7 +46,7 @@ sim1cycle <- function(sys, ctypes, compfts, n0y0, beta, fts, tnowstep, hor, tpre
           # calculate current taustar
           gnowvec <- gnowhor(signnow, n0y0[wctnow], beta[wctnow], ftsnow[wctnow], tnow, hor = hor,
                              seqlen = seqlen, prior = prior, cu = cu, cp = cp, onecycle = onecycle)
-          taustarnow <- gnowvec$tau[which.min(gnowvec$gnow)]
+          taustarnow <- gnowvec$tau[which.min(gnowvec$gnow)] - tnow
           if (taustarnow <= tprep){ # schedule repair for tnow + tprep if not scheduled already
             repschedfor <- tnow + tprep
           } # else do nothing & go on to next loop (repschedfor is still Inf)
@@ -57,6 +58,7 @@ sim1cycle <- function(sys, ctypes, compfts, n0y0, beta, fts, tnowstep, hor, tpre
       taustarnow <- NA
     }
     # now we know if failed or not, if and when repair scheduled
+    cat("failed =", failed, "taustar =", taustarnow, "repschedfor =", repschedfor, "\n")
     if (tnow >= repschedfor){ # time for repair has come, operational cycle ends
       gotonext <- FALSE
     }
@@ -65,20 +67,27 @@ sim1cycle <- function(sys, ctypes, compfts, n0y0, beta, fts, tnowstep, hor, tpre
     res <- rbind(res, resnow)
     # now prepare for next loop
     tnow <- tnow + tnowstep
-    # update stuff if not failed (otherwise not needed except tnow update!)
+    # update stuff if system not failed (otherwise not needed except tnow update!)
     if (!failed){
       failedcompsnow <- ftschron[ftschron <= tnow]
-      sysnow <- induced_subsystem(sysnow, vids=V(sys)[!(name %in% names(failedcompsnow))])
-      signnow <- computeSystemSurvivalSignature(sysnow)
-      # to which fts list element do the failure times in failedcompsnow belong?
-      ftsnowindex <- sapply(ctypes, function(ctypesl) names(failedcompsnow) %in% ctypesl)
-      ftsnowindex <- apply(ftsnowindex, 1, which)
-      # write failure times in corresponding list element
-      for (k in 1:K)
-        ftsnow[[k]] <- failedcompsnow[ftsnowindex == k]
-      # which component types are now present? (subset of 1:K)
-      wctnow <- which(names(ctypes) %in% names(signnow))
-    } # end update if not failed
+      if (length(failedcompsnow) > 0){ # update stuff only when the first component has failed
+        # recalculates everything from scratch (do this in a more clever way?)
+        sysnow <- induced_subgraph(sys, vids=V(sys)[!(name %in% names(failedcompsnow))])
+        # TODO: catch error when sysnow now contains vertices s and t only (suddenly all components fail) 
+        signnow <- computeSystemSurvivalSignature(sysnow)
+        # to which fts list element do the failure times in failedcompsnow belong?
+        ftsnowindex <- sapply(ctypes, function(ctypesl) names(failedcompsnow) %in% ctypesl)
+        if (length(failedcompsnow) == 1)
+          ftsnowindex <- which(ftsnowindex)
+        else
+          ftsnowindex <- apply(ftsnowindex, 1, which)
+        # write failure times in corresponding list element
+        for (k in 1:K)
+          ftsnow[[k]] <- failedcompsnow[ftsnowindex == k]
+        # which component types are now present? (subset of 1:K)
+        wctnow <- which(names(ctypes) %in% names(signnow))
+      }
+    } # end update if system not failed
   } # end while loop
   # update Weibull parameters (all component types!) for next operational cycle using nnynlist() (to be defined)
   # return res and all, tend = last.tnow + trepa, unit cost rate = (cp or cu) / last.tnow
