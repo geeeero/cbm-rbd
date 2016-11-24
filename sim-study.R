0636565f569ebe871ea31a62e4401839cb96c807
Gero Walter <g.m.walter@tue.nl>
2016-11-24 18:33:19 +0100

    third simulation

--- sim-study.R
+++ sim-study.R
@@ -147,9 +147,10 @@ br1sim2fig4
 dev.off()
 
 br1sim2fig5 <- ggplot(melt(br1sim2summaryall, c("id", "sim")), aes(x = id, y = value)) +
-  geom_line(aes(group = sim)) + geom_point(aes(group = sim)) +
+  geom_line(aes(group = sim)) + geom_point(aes(group = sim)) + 
   facet_grid(variable ~ sim, scales = "free_y", labeller = simlabeller2) +
-  xlab("5-cycle repetition number") + theme(axis.title.y = element_blank())
+  xlab("5-cycle repetition number") + theme(axis.title.y = element_blank()) +
+  geom_hline(yintercept = median(value))
 pdf("br1sim2fig5.pdf", width = 6, height = 6)
 br1sim2fig5
 dev.off()
@@ -157,6 +158,40 @@ dev.off()
 # sim 3: failures later than expected
 # -----------------------------------
 
+# simulate 20 5-cycle machines
+set.seed(2411)
+br1sim5cycle20data3 <- list()
+for (i in 1:20){
+  br1sim5cycle20data3[[i]] <- brWeibullData(5, br1beta, 2*br1mttf)
+}
 
+br1sim3 <- list()     # our model
+br1sim3pr <- list()   # do not update params during cycle, but at end of cycle 
+br1sim3prpr <- list() # never update params
+
+for (i in 1:20){
+  cat("Repetition", i, ": full update\n")
+  br1sim3[[i]] <- simNcycle(sys = br, ctypes = brctypes, compfts = br1sim5cycle20data3[[i]], n0y0 = br1n0y0,
+                            beta = br1beta, tnowstep = 0.1, hor = 4, thresh = 0.5, seqlen = 401)
+  cat("Repetition", i, ": end of cycle update only\n")
+  br1sim3pr[[i]] <- simNcycle(sys = br, ctypes = brctypes, compfts = br1sim5cycle20data3[[i]], n0y0 = br1n0y0,
+                              beta = br1beta, tnowstep = 0.1, hor = 4, thresh = 0.5, seqlen = 401, prior = TRUE)
+  cat("Repetition", i, ": no update\n")
+  br1sim3prpr[[i]] <- simNcycle(sys = br, ctypes = brctypes, compfts = br1sim5cycle20data3[[i]], n0y0 = br1n0y0,
+                                beta = br1beta, tnowstep = 0.1, hor = 4, thresh = 0.5, seqlen = 401, prior = TRUE, cycleupdate = FALSE)
+}
+
+br1sim3summary <- simsummary(br1sim3)
+br1sim3prsummary <- simsummary(br1sim3pr)
+br1sim3prprsummary <- simsummary(br1sim3prpr)
+
+br1sim3fig5 <- ggplot(melt(br1sim3summaryall, c("id", "sim")), aes(x = id, y = value)) +
+  geom_line(aes(group = sim)) + geom_point(aes(group = sim)) + 
+  facet_grid(variable ~ sim, scales = "free_y", labeller = simlabeller2) +
+  xlab("5-cycle repetition number") + theme(axis.title.y = element_blank()) +
+  geom_hline(yintercept = median(value))
+pdf("br1sim3fig5.pdf", width = 6, height = 6)
+br1sim3fig5
+dev.off()
 
 #
