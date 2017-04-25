8038325b50297f0776e6b3c9857961cbda83407b
Gero Walter <gero@gmx.at>
2017-04-25 12:47:11 +0200

    update package requirements for Lisa

--- extsim-setup.R
+++ extsim-setup.R
@@ -4,12 +4,21 @@
 
 # install packages if necessary
 install.packages(c("actuar", "igraph", "ReliabilityTheory", "reshape2"))
-# on LISA, need to install "actuar", "igraph", "ReliabilityTheory"
+# on LISA, if they install "actuar", "igraph", and "mcmc",
+# then I need to install only "ReliabilityTheory",
+# which depends on the below other three packages:
+install.packages("FRACTION")
+install.packages("PhaseType")
+install.packages("HI")
+install.packages("ReliabilityTheory")
+
+
 
 # load packages
 library(actuar)
 library(ReliabilityTheory)
 library(reshape2) # might not be necessary
+#library(plyr)
 
 # code files
 source("weibull-sys-functions.R") # contains also plotting functions that work only with ggplot2 and gridExtra
